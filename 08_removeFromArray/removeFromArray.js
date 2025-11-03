3e9b8801a0959d95c98856f488ed51c6efa36bbd
Gokul Ram <gokulram.ramesh@redingtongroup.com>
2025-11-03 17:13:02 +0530

    Complete exercise 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    //check for no arguments
+    if(arguments.length === 0) {
+        return;
+    }
+    //check for only one argument
+    if(arguments.length === 1) {
+        return array;
+    }
+    const args = Array.from(arguments)
+    const valuesToRemove = args.slice(1);
+    // valuesToRemove.forEach(value => {
+    //     const indexToRemove = array.indexOf(value);
+    //     if(indexToRemove !== -1) {
+    //         array.splice(indexToRemove, 1);
+    //     }
+    // });
+    let result = array.filter((value) => {
+        return !valuesToRemove.includes(value);
+    });
+    return result;
 };
 
 // Do not edit below this line
