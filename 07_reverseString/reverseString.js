442a2c9ad8eadfa8332ab7b71b4331aac41c46b3
Gokul Ram <gokulram.ramesh@redingtongroup.com>
2025-11-03 16:30:59 +0530

    Complete exercise 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let result = "";
+    for(let index =-1; index >= -str.length; index--) {
+        result += str.at(index);
+    }
+    return result;
+    //return str.split("").reverse().join('');
 };
 
 // Do not edit below this line
