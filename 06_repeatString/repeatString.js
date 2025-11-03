538694bbaa3328016f84ba4a9e67d4a0e8f9c744
Gokul Ram <gokulram.ramesh@redingtongroup.com>
2025-11-03 15:37:18 +0530

    Complete exercise 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+    if(count < 0) {
+        return 'ERROR';
+    }
+    let result = "";
+    for(let index=0; index < count; index++) {
+        result += str;
+    }
+    return result;
 };
 
 // Do not edit below this line
