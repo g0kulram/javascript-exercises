456f2dcd1580b12931f0f50f7aadf3c39474eb5b
Gokul Ram <gokulram.ramesh@redingtongroup.com>
2025-11-18 17:54:23 +0530

    Complete exercise 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    return peopleArray.reduce((oldest, person) => {
+        const lastYear = (person.yearOfDeath == undefined) ? 2025 : person.yearOfDeath;
+        const age = lastYear - person.yearOfBirth;
 
+        const oldestLastYear = (oldest.yearOfDeath == undefined) ? 2025 : oldest.yearOfDeath;
+        const oldestAge = oldestLastYear - oldest.yearOfBirth;
+        return age > oldestAge ? person : oldest;
+    }, peopleArray[0]);
 };
 
 // Do not edit below this line
