1edb078713ed1b2f0d981b7fe711ec4800b0f3e1
Gokul Ram <gokulram.ramesh@redingtongroup.com>
2025-11-18 17:30:15 +0530

    Complete exercise 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    if(position < 0) {
+        return 'OOPS';
+    }
+    if(position == 0) {
+        return 0;
+    }
+    if(position == 1 || position == 2) {
+        return 1;
+    }
+    let nMinus1 = 1;
+    let n = 1;
+    for(let i = 3; i <= position; i++) {
+        const temp = n;
+        n += nMinus1;
+        nMinus1 = temp;
+    }
+    return n;
 };
 
 // Do not edit below this line
